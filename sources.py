0dabd65dc8be80c170cfdef6745defab03c9e6fa
Eugene <firsovghenya@gmail.com>
2025-03-02 01:49:53 +0300

    added VK publics sources

---
+++ sources.py
@@ -0,0 +1,9 @@
+VK_PUBLICS = {
+    'murmansk_obl' : 'murman_sever',
+    'murmansk' : 'murmanskgroup',
+    'podslushano' : 'murman_51',
+    'murmansk2' : 'i.murmansk',
+    'trevoga' : 'public181923765',
+    'reginfo' : 'reginfo51',
+    'murmansk3' : 'public195569983'
+}
