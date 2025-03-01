154caae473edc74a01188f22bbddda975ee969fb
Eugene <firsovghenya@gmail.com>
2025-03-02 01:49:39 +0300

    added VK publics sources

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
 import requests
 import os
+
+from sources import VK_PUBLICS
+
 load_dotenv()
 domain ="club224630814"
 #request = f"https://api.vk.com/method/wall.get?access_token={os.getenv("VK_API_TOKEN")}&v={os.getenv("VK_API_VERSION")}&domain={domain}"
