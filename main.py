dd495118e31a8a021af342cd0c467c689f762d9b
Eugene <firsovghenya@gmail.com>
2025-03-02 01:26:50 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+import requests
+import os
+load_dotenv()
+domain ="club224630814"
+#request = f"https://api.vk.com/method/wall.get?access_token={os.getenv("VK_API_TOKEN")}&v={os.getenv("VK_API_VERSION")}&domain={domain}"
+
+response = requests.get('https://api.vk.com/method/wall.get', 
+                        params={
+                            'access_token': os.getenv("VK_API_TOKEN"),
+                            'v': os.getenv("VK_API_VERSION"),
+                            'domain' : domain
+                        }
+                        )
+data = response.json()['response']['items']
+print(data)
