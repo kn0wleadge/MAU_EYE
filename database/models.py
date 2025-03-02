8e449be7da63583a9eb4520debae9e6ab417b1f6
Eugene <firsovghenya@gmail.com>
2025-03-03 01:06:00 +0300

    added insert function into VK_POSTS_RAW table

---
+++ database/models.py
@@ -0,0 +1,47 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+from sqlalchemy import BigInteger, Integer, String,DateTime, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+from sqlalchemy import select, insert, update
+
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
+class VkPostsRaw(Base):
+    __tablename__ = 'vk_posts_raw'
+    pid:Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    ptext:Mapped[str] = mapped_column(String(15895))
+    purl:Mapped[str] = mapped_column(String(150))
+    pparsetime = mapped_column(DateTime)
+    pgroup:Mapped[str] = mapped_column(String(48))
+    def __str__(self):
+        return f"VkPostsRaw(pid={self.pid}, text={self.ptext}, url={self.purl}, parse_time={self.pparsetime}, group={self.pgroup})"
+    
+engine = create_async_engine(url=os.getenv("POSTGRESQL_URL"))
+async_session = async_sessionmaker(engine)
+
+async def async_main():
+    async with engine.begin() as conn:
+        #await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+async def insert_post(text:str, url:str, parse_time:DateTime, group:str):
+    async with async_session() as session:
+        try:
+            await session.execute(insert(VkPostsRaw).values(ptext = text, purl = url, pparsetime = parse_time, pgroup = group))
+            await session.commit()
+        except Exception as e:
+            print(f'Error during insterting into VkPostsRaw - {e}')
+
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -4,33 +4,16 @@ import requests
 import asyncio
 import os
 import datetime
-from sqlalchemy import BigInteger, Integer, String,DateTime, ForeignKey
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
-from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
-from sqlalchemy import select, insert, update
-class Base(AsyncAttrs, DeclarativeBase):
-    pass
-class VkPostsRaw(Base):
-    __tablename__ = 'vk_posts_raw'
-    pid:Mapped[int] = mapped_column(primary_key=True)
-    ptext:Mapped[str] = mapped_column(String(15895))
-    purl:Mapped[str] = mapped_column(String(150))
-    pparsetime = mapped_column(DateTime)
-    pgroup:Mapped[str] = mapped_column(String(48))
-    def __str__(self):
-        return f"VkPostsRaw(pid={self.pid}, text={self.ptext}, url={self.purl}, parse_time={self.pparsetime}, group={self.pgroup})"
 
+from database.models import async_main
+from database.models import async_session
+from database.models import insert_post
 from sources import VK_PUBLICS
 #request = f"https://api.vk.com/method/wall.get?access_token={os.getenv("VK_API_TOKEN")}&v={os.getenv("VK_API_VERSION")}&domain={domain}"
 
 
-engine = create_async_engine(url=os.getenv("POSTGRESQL_URL"))
-async_session = async_sessionmaker(engine)
-async def async_main():
-    async with engine.begin() as conn:
-        #await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
+
+
 
 
 async def get_posts(domain, count):
@@ -53,9 +36,8 @@ async def get_posts(domain, count):
 
 async def main():
     await async_main()
-    async with async_session() as session:
-        data = await session.scalar(select(VkPostsRaw).where(VkPostsRaw.pid == 1))
-        print(data)
+    await insert_post("TEXT", "URL", datetime.datetime.now(),"GROUP")
+
 
 if __name__ == '__main__':
 
